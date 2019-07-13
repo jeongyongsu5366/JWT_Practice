cfaf29b147308340617dfead5eb01085e08bfad8
su <jeongyongsu5366@gmail.com>
2019-07-14 04:35:49 +0900

    Validation with Joi
    1. Test out valid and invalide case

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,40 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
-// When we go to register
 // Validation with JOi
+const Joi = require('@hapi/joi');
+
+// This schema is going to be used for validation
+const schema = {
+	name: Joi.string()
+		.min(6)
+		.required(),
+	email: Joi.string()
+		.min(6)
+		.required()
+		.email(),
+	password: Joi.string()
+		.min(6)
+		.required()
+};
+
 // /api/user/register
 router.post('/register', async (req, res) => {
+	// Let's Validate The Data before we add user
+	// It will return an object
+
+	// const validation = Joi.validate(req.body, schema);
+	// res.send(validation)
+
+	// As we only want to get error message we can destructure it
+	const { error } = Joi.validate(req.body, schema);
+	// res.send(error.details[0].message);
+	if (error) return res.status(400).send(error.details[0].message);
+
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
-		password: req.body.password,
-		date: req.body.date
+		password: req.body.password
 	});
 
 	try {
@@ -21,9 +46,26 @@ router.post('/register', async (req, res) => {
 	}
 });
 
-// /api/user/login
-// router.post('/login', (req, res) => {
-// 	res.send();
-// });
+// Joi.validate Result, and put invalid value
+// {
+// 	"name" : "SU JEONG",
+// 	"email": "akh5366@gmail.com",
+// 	"password": "123456"
+// }
 
+// {
+// 	"error": null,
+// 	"value": {
+// 			"name": "SU JEONG",
+// 			"email": "akh5366@gmail.com",
+// 			"password": "123456"
+// 	}
+// }
+
+// Error Case
+// {
+// 	"name" : "SUG",
+// 	"email": "akh5366@gmail.com",
+// 	"password": "123456"
+// }
 module.exports = router;
