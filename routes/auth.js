74e08cab4f98bb4ed74f85c3698722118cafa9ea
su <jeongyongsu5366@gmail.com>
2019-07-14 05:05:41 +0900

    Hashing Passwords and Setting up the login route
    - In Login Route, I check the email from the user input and if the email
    is matched with the exact same eamil in the DB. I am going to use
    bcrypt.compare() method for checking if the user enters a valid password

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const { registerValidation } = require('../validation');
+const { registerValidation, loginValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
 // /api/user/register
 router.post('/register', async (req, res) => {
 	const { error } = registerValidation(req.body);
@@ -10,42 +11,69 @@ router.post('/register', async (req, res) => {
 	const emailExist = await User.findOne({ email: req.body.email });
 	if (emailExist) return res.status(400).send('Email already exists');
 
+	// Hash the passwords
+	// The complexity of the string that this has algorithm get generated
+	const salt = await bcrypt.genSalt(10);
+	// This salt will be combined with the hashed password
+	const hashPassword = await bcrypt.hash(req.body.password, salt);
+
 	// Create a new User
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
-		password: req.body.password
+		password: hashPassword
+		// password: req.body.password
 	});
 
 	try {
 		// Store on DB
 		const savedUser = await user.save();
-		res.send(savedUser);
+		// res.send(savedUser);
+		res.send({ user: user._id });
 	} catch (err) {
 		res.status(400).send(err);
 	}
 });
 
-// Joi.validate Result, and put invalid value
-// {
-// 	"name" : "SU JEONG",
-// 	"email": "akh5366@gmail.com",
-// 	"password": "123456"
-// }
+//Login
+router.post('/login', async (req, res) => {
+	const { error } = loginValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
-// {
-// 	"error": null,
-// 	"value": {
-// 			"name": "SU JEONG",
-// 			"email": "akh5366@gmail.com",
-// 			"password": "123456"
-// 	}
-// }
+	// Checking if the email exists
+	const user = await User.findOne({ email: req.body.email });
+	if (!user) return res.status(400).send('Email or password is wrong');
+	// Password is correct
+	const validPass = await bcrypt.compare(req.body.password, user.password);
 
-// Error Case
-// {
-// 	"name" : "SUG",
-// 	"email": "akh5366@gmail.com",
-// 	"password": "123456"
-// }
+	if (!validPass) return res.status(400).send('Invalid Password');
+
+	res.send('Logged in!');
+});
+
 module.exports = router;
+
+// {
+// 	"name" : "Heather",
+// 	"email": "jojos55151@gmail.com",
+// 	"password": "oo101010"
+// }
+
+// We can find the hased password
+// {
+// 	"_id": "5d2a36cc73bf665dc86e031c",
+// 	"name": "Heather",
+// 	"email": "jojos55151@gmail.com",
+// 	"password": "$2a$10$Vt/jvviQ2vOir4uuwEwhN.ciVN2D.ZJafXz/oxnbYSaT5k6nhvXc6",
+// 	"date": "2019-07-13T19:53:48.941Z",
+// 	"__v": 0
+// }
+
+// -------------------------------
+// Test Case
+// {
+// 	"email": "jos5366@gmail.com",
+// 	"password": "oo101010"
+// }
+
+// Logged in!
